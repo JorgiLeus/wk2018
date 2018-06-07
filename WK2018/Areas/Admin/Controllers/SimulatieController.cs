9e6ffd03f309be1f93bf66d88de67ae2ba246f7f
c1042421@hik.be <c1042421@hik.be>
2018-06-07 19:00:13 +0200

    temp

---
+++ WK2018/Areas/Admin/Controllers/SimulatieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WK2018.Areas.Admin.Controllers
+{
+    public class SimulatieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
